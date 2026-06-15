3b990cfa46d2a391aff05c8fa4420a6ec1cf5f4f
tomjero17-boop <tomjero17@gmail.com>
2026-06-15 16:07:27 -0400

    updating Product Survey file

--- productSurvey/product_Survey.js
+++ productSurvey/product_Survey.js
@@ -1,16 +1,14 @@
-
-
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
 const submitButton=document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
 
 function submitFeedback() { 
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
 
     alert('Thank you for your valuable feedback')
 
@@ -22,13 +20,10 @@ function submitFeedback() {
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
     document.getElementById('userInfo').style.display = 'block';
-
 }
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-      submitFeedback();
+        submitFeedback();
     }
 });
-
-  
